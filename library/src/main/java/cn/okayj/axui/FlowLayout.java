6602da4744203a05e705b20ecfa7558dc3a625cb
jack <497737103@qq.com>
2016-08-21 03:32:18 +0800

    增加FlowLayout

--- library/src/main/java/cn/okayj/axui/FlowLayout.java
+++ library/src/main/java/cn/okayj/axui/FlowLayout.java
@@ -5,7 +5,12 @@ import android.content.Context;
 import android.graphics.drawable.Drawable;
 import android.support.annotation.IntDef;
 import android.util.AttributeSet;
+import android.view.Gravity;
+import android.view.View;
 import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by huangkaijie on 16/8/20.
@@ -17,11 +22,11 @@ public class FlowLayout extends ViewGroup {
     public static final int HORIZONTAL = 0;
     public static final int VERTICAL = 1;
     
-    private static final int BAND_WIDTH_NOT_FIXED = -1;
+    private static final int BAND_WIDTH_NOT_FIXED = 0;
 
     private int mOrientation = HORIZONTAL;
-    private boolean mReverse = false;
-    
+    private int mGravity = Gravity.START | Gravity.TOP;
+
     private int mBandWidth = BAND_WIDTH_NOT_FIXED;
 
     private Drawable mDivider;
@@ -31,6 +36,8 @@ public class FlowLayout extends ViewGroup {
     private int mDividerHeight;
     private int mGapWidth;
     private int mGapHeight;
+
+    private List<Band> mBands = new ArrayList<>();
 
     public FlowLayout(Context context) {
         this(context,null);
@@ -57,7 +64,206 @@ public class FlowLayout extends ViewGroup {
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        // TODO: 16/8/20  
+        mBands.clear();
+
+
+
+        int width = 0;
+        int height = 0;
+
+        int flowLength = 0;//flow orientation dimension
+        int flowWidth = 0;
+
+
+        final int widthMode = MeasureSpec.getMode(widthMeasureSpec);
+        final int heightMode = MeasureSpec.getMode(heightMeasureSpec);
+        final int widthSize = MeasureSpec.getSize(widthMeasureSpec);
+        final int heightSize = MeasureSpec.getSize(heightMeasureSpec);
+
+        final int flowLengthMode;
+        final int flowLengthSize;
+        final int flowWidthMode;
+        final int flowWidthSize;
+
+        if(mOrientation == HORIZONTAL){
+            flowLengthMode = widthMode;
+            flowLengthSize = widthSize;
+            flowWidthMode = heightMode;
+            flowWidthSize = heightSize;
+        }else {
+            flowLengthMode = heightMode;
+            flowLengthSize = heightSize;
+            flowWidthMode = widthMode;
+            flowWidthSize = widthSize;
+        }
+
+        switch (flowLengthMode){
+            case MeasureSpec.EXACTLY:
+                flowLength = flowLengthSize;
+                break;
+        }
+
+        switch (flowWidthMode){
+            case MeasureSpec.EXACTLY:
+                flowWidth = flowLengthSize;
+                break;
+        }
+
+        Band band;
+        int bandWidth = 0;
+        int bandLength = 0;
+        boolean newBand = true;
+        int childCount = getChildCount();
+        for(int i = 0; i < childCount; ++i){
+            View child = getChildAt(i);
+            LayoutParams lp = (LayoutParams) child.getLayoutParams();
+
+            if(newBand){
+                bandLength = 0;
+                band = new Band();
+                band.setStartIndex(0);
+                if(mBandWidth != BAND_WIDTH_NOT_FIXED)
+                    band.setBandWidth(mBandWidth);
+                mBands.add(band);
+                newBand = false;
+            }
+            band = mBands.get(mBands.size() - 1);
+
+            if(flowLengthMode == MeasureSpec.EXACTLY){
+                int preBandLength = bandLength;
+                bandLength +=
+            }
+        }
+
+
+
+        // TODO: 16/8/20
+    }
+
+    void measureHorizontal(int widthMeasureSpec, int heightMeasureSpec) {
+        int width = 0;
+        int height = 0;
+
+        final int widthMode = MeasureSpec.getMode(widthMeasureSpec);
+        final int heightMode = MeasureSpec.getMode(heightMeasureSpec);
+        final int widthSize = MeasureSpec.getSize(widthMeasureSpec);
+        final int heightSize = MeasureSpec.getSize(heightMeasureSpec);
+        
+        boolean fixedBandWidth = mBandWidth != BAND_WIDTH_NOT_FIXED;
+
+        Band band;
+        int bandWidth = 0;
+        int bandLength = 0;
+        int preBandLength = bandLength;
+        int tempBandLength = 0;
+        boolean newBand = true;
+        int childCount = getChildCount();
+        for(int i = 0; i < childCount - 1; ++i){
+            View child = getChildAt(i);
+            if(child.getVisibility() == GONE){
+                continue;//当它不存在,应该不会有影响
+            }
+            
+            LayoutParams lp = (LayoutParams) child.getLayoutParams();
+
+            if(widthMode == MeasureSpec.EXACTLY){
+                if(!newBand && widthSize < bandLength + lp.leftMargin + lp.rightMargin){
+                    //因为margin而放不下,需要另起一行。
+                    
+                    newBand = true;
+                    preBandLength = bandLength;
+                    bandLength = 0;
+                }
+
+                tempBandLength += lp.leftMargin + lp.rightMargin;
+                int wMeasureSpec;
+                if(lp.width == LayoutParams.MATCH_PARENT){
+                    wMeasureSpec = MeasureSpec.makeMeasureSpec(widthSize - tempBandLength,MeasureSpec.EXACTLY);
+                }else {
+                    wMeasureSpec = MeasureSpec.makeMeasureSpec(widthSize - tempBandLength,MeasureSpec.AT_MOST);
+                }
+                int hMeasureSpec;
+                if(fixedBandWidth){
+                        /*
+                        用带宽去限制child的高度
+                         */
+
+                    if(lp.height == LayoutParams.MATCH_PARENT){
+                        hMeasureSpec = MeasureSpec.makeMeasureSpec(mBandWidth - lp.topMargin - lp.bottomMargin,MeasureSpec.EXACTLY);
+                    }else{
+                        hMeasureSpec = MeasureSpec.makeMeasureSpec(mBandWidth - lp.topMargin - lp.bottomMargin,MeasureSpec.AT_MOST);
+                    }
+                }else {
+                        /*
+                        用父试图的高度限制
+                         */
+
+                    hMeasureSpec = MeasureSpec.makeMeasureSpec(heightSize - lp.topMargin - lp.bottomMargin,heightMode);
+                }
+                child.measure(wMeasureSpec,hMeasureSpec);
+                
+                boolean reMeasure = false;
+                if(!newBand && bandLength + child.getMeasuredWidth() > widthSize){
+                    reMeasure = true;
+                    newBand = true;
+                    preBandLength = bandLength;
+                    bandLength = 0;
+                }
+                
+                if(reMeasure){
+                    wMeasureSpec = MeasureSpec.makeMeasureSpec(widthSize,MeasureSpec.EXACTLY);
+                    child.measure(wMeasureSpec,hMeasureSpec);
+                }
+            }else if(widthMode == MeasureSpec.UNSPECIFIED){
+                // TODO: 16/8/21  
+            }else {
+                // TODO: 16/8/21  
+            }
+            
+            
+            if(newBand){
+                bandLength = 0;
+                band = new Band();
+                band.setStartIndex(i);
+                mBands.add(band);
+                newBand = false;
+            }
+        }
+        
+        
+        
+        /*
+        *计算宽高,并计算带宽
+         */
+        int contentHeight = 0;
+        int contentWidth = 0;
+        int start = 0;
+        int end = 0;
+        band = null;
+        for(int i = 0; i < mBands.size() - 1; ++i){
+            band = mBands.get(i);
+            start = band.getStartIndex();
+            if(i < mBands.size() - 1){
+                end = mBands.get(i + 1).getStartIndex();
+            }else {
+                end = getChildCount() - 1;
+            }
+
+            int maxChildHeight = 0;
+            bandLength = 0;
+            for(int childIndex = start; childCount <= end; ++childCount){
+                View child = getChildAt(childIndex);
+                if(child.getVisibility() == GONE)
+                    continue;
+                maxChildHeight = Math.max(maxChildHeight,child.getMeasuredHeight());
+                bandLength += child.getMeasuredWidth();
+            }
+            band.setBandWidth(maxChildHeight);
+            contentWidth = Math.max(contentWidth,bandLength);
+            contentHeight += maxChildHeight;
+        }
+        
+        if(widthMode == )// TODO: 16/8/21  
     }
 
     @Override
@@ -84,14 +290,22 @@ public class FlowLayout extends ViewGroup {
             requestLayout();
         }
     }
-    
-    public void setReverse(boolean reverse){
-        if(mReverse != reverse){
-            mReverse = reverse;
+
+    public void setGravity(int gravity){
+        if (mGravity != gravity) {
+            if ((gravity & Gravity.RELATIVE_HORIZONTAL_GRAVITY_MASK) == 0) {
+                gravity |= Gravity.START;
+            }
+
+            if ((gravity & Gravity.VERTICAL_GRAVITY_MASK) == 0) {
+                gravity |= Gravity.TOP;
+            }
+
+            mGravity = gravity;
             requestLayout();
         }
     }
-
+    
     public void setDividerDrawable(Drawable divider) {
         if (divider == mDivider) {
             return;
@@ -148,6 +362,27 @@ public class FlowLayout extends ViewGroup {
             super(source);
 
             //// TODO: 16/8/20
+        }
+    }
+
+    private static class Band {
+        private int mBandWidth;
+        private int mStartIndex;//start child index
+
+        public int getBandWidth() {
+            return mBandWidth;
+        }
+
+        public void setBandWidth(int bandWidth) {
+            this.mBandWidth = bandWidth;
+        }
+
+        public int getStartIndex() {
+            return mStartIndex;
+        }
+
+        public void setStartIndex(int startIndex) {
+            this.mStartIndex = startIndex;
         }
     }
 }
